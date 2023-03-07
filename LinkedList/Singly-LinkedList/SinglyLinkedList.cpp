a8e9603a99bea0a10a4f983cc2a250bec0d0aca7
Shan-Lun-1 <S10222855@connect.np.edu.sg>
2023-03-07 12:00:33 +0800

    Added Singly Linked List

--- LinkedList/Singly-LinkedList/SinglyLinkedList.cpp
+++ LinkedList/Singly-LinkedList/SinglyLinkedList.cpp
@@ -1,6 +1,95 @@
 #include "SinglyLinkedList.h"
-#include <forward_list>
+
+bool SinglyLinkedList::add(ItemType item) {
+    list.push_front(item);
+    return true;
+}
+
+bool SinglyLinkedList::add(int index, ItemType item) {
+    if (index < 0 || index >= getLength())
+        return false;
+
+    auto it = list.before_begin();
+    for (int i = 0; i < index; i++)
+        ++it;
+
+    list.insert_after(it, item);
+    return true;
+}
+
+void SinglyLinkedList::remove(int index) {
+    if (index < 0 || index >= getLength())
+        return;
+
+    auto it = list.before_begin();
+    for (int i = 0; i < index; i++)
+        ++it;
+
+    list.erase_after(it);
+}
+
+ItemType SinglyLinkedList::get(int index) {
+    if (index < 0 || index >= getLength())
+        throw out_of_range("Index out of range");
+
+    auto it = list.begin();
+    for (int i = 0; i < index; i++)
+        ++it;
+
+    return *it;
+}
+
+bool SinglyLinkedList::isEmpty() {
+    return list.empty();
+}
+
+int SinglyLinkedList::getLength() {
+    int count = 0;
+    for (auto it = list.begin(); it != list.end(); ++it)
+        count++;
+
+    return count;
+}
+
+void SinglyLinkedList::print() {
+    for (auto it = list.begin(); it != list.end(); ++it)
+        cout << *it << " ";
+    cout << endl;
+}
 
 int singly_linked_list_main() {
+    SinglyLinkedList list;
 
+    // add elements
+    list.add("1");
+    list.add("2");
+    list.add("3");
+    list.add("4");
+    list.add("5");
+
+    // print the list
+    list.print();
+
+    // insert an element at index 2
+    list.add(2, "6");
+
+    // print the list again
+    list.print();
+
+    // remove an element at index 4
+    list.remove(4);
+
+    // print the list again
+    list.print();
+
+    // get the element at index 3 and print it
+    cout << list.get(3) << endl;
+
+    // print the length of the list
+    cout << list.getLength() << endl;
+
+    // check if the list is empty
+    cout << boolalpha << list.isEmpty() << endl;
+
+    return 0;
 }
--- LinkedList/Singly-LinkedList/SinglyLinkedList.h
+++ LinkedList/Singly-LinkedList/SinglyLinkedList.h
@@ -1,1 +1,27 @@
+#ifndef PIOI_WORKSHOP_SINGLYLINKEDLIST_H
+#define PIOI_WORKSHOP_SINGLYLINKEDLIST_H
+
+#include <forward_list>
+#include <iostream>
+#include <string>
+using namespace std;
+
+typedef string ItemType;
+
+class SinglyLinkedList {
+private:
+    forward_list<ItemType> list;
+
+public:
+    bool add(ItemType item);
+    bool add(int index, ItemType item);
+    void remove(int index);
+    ItemType get(int index);
+    bool isEmpty();
+    int getLength();
+    void print();
+};
+
 int singly_linked_list_main();
+
+#endif
